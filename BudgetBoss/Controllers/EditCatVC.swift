c430483c2cb25c361e2a51ae0dab76b718de802f
Adolfo Frias <adf44@pitt.edu>
2018-11-04 15:27:31 -0500

    committing EditCatVC

---
+++ BudgetBoss/Controllers/EditCatVC.swift
@@ -0,0 +1,32 @@
+//
+//  editCatVC.swift
+//  BudgetBoss
+//
+//  Created by Adolfo Frias on 11/4/18.
+//  Copyright © 2018 Adolfo Frias. All rights reserved.
+//
+
+import UIKit
+
+class EditCatVC: UIViewController {
+
+    var catToEdit: ObjCategory?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
